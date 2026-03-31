71c975f0d20fe409e43632d6707dacfab5efacd7
Lanny MacMillan <lannymacmillan@gmail.com>
2026-03-30 22:31:18 -0400

    refactor: update availableHolidays to use holidayData instead of filteredHolidays

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -75,7 +75,7 @@ export default function HomeContent({ homeData }: HomeContentProps) {
         description="Plan your holidays, stay organized, and have fun!"
         showBackButton={false}
         cycleIcons={true}
-        availableHolidays={filteredHolidays.map(h => h.id)}
+        availableHolidays={holidayData.map(h => h.id)}
       />
       <main className="flex-1 w-full max-w-4xl flex flex-col gap-6 mt-4">
         {filteredHolidays.length === 0 ? (
